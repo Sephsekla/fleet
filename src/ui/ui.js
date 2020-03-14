ff7d39dd52842359a170f93a65a9b0bb3bbe2c7c
Joe Bailey-Roberts <dev@joebr.io>
2020-03-14 15:14:59 +0000

    Remove unneeded jquery

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,18 +1,24 @@
-import $ from 'jquery';
 class ui{
     constructor(){
 
     }
 
+    static setBar(id,percent){
+        let elem = document.querySelector(`#${id} span`);
+        elem.style.width = `${percent}%`;
+    }
+
     static setShieldBar(percent){
-        $('#shields span').css('width',`${percent}%`);
+        this.setBar('shields',percent)
     }
     static setHullBar(percent){
-        $('#hull span').css('width',`${percent}%`);
+        this.setBar('hull',percent)
     }
     static setEngineBar(percent){
-        $('#engines span').css('width',`${percent}%`);
+        this.setBar('engines',percent)
     }
+
+
 }
 
 export default ui;
